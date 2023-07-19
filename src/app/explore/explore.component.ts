143fd7ef143caf8cba3441f3866362984bb14778
EmpiricalCode <daniellongschool@gmail.com>
2023-07-19 11:08:45 -0400

    Added custom page titles to routing

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-explore',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class ExploreComponent {
 
+  constructor(private titleService: Title) { 
+    this.titleService.setTitle("RideWise - Explore");
+  }
 }
