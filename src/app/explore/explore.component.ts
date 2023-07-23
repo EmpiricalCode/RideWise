637d2e5bd690014bb0c9f1ef85264cfdcb87c29b
empiricalcode <daniellongschool@gmail.com>
2023-07-23 01:23:15 -0400

    Integrated tomtom map

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import * as tt from '@tomtom-international/web-sdk-maps';
 
 @Component({
   selector: 'app-explore',
@@ -10,5 +11,13 @@ export class ExploreComponent {
 
   constructor(private titleService: Title) { 
     this.titleService.setTitle("RideWise - Explore");
+  }
+
+  ngOnInit() {
+    const map = tt.map({
+      key: "ImJQ5OE7KBtQRP09rOL4mQXtlKm4qydm",
+      container: "map",
+    })
+    map.addControl(new tt.NavigationControl())
   }
 }
